7a5cb4ecd87628b584833468fd0fdc64962d2123
nawawn <nawawn@googlemail.com>
2019-08-18 16:29:25 +0100

    updated dsc script

--- DSC/IIS-Dsc.ps1
+++ DSC/IIS-Dsc.ps1
@@ -1,23 +1,4 @@
-﻿$html = @'
-<html>
-    <head><title>Azure Infrastructure</title>
-        <style>
-            .center{
-                margin: auto; width: 50%; border: 4px solid #93BD25; padding: 10px;
-            }
-        </style>
-    </head>
-    <body>
-    <div class="center" style="background-color:lightgray;">
-        <h3>A Journey to One Click of a Button</h3><hr />
-        <p>Or is it rather two clicks with <button type="button">Queue</button> and <button type="button"> Run </button></p>
-        <p>Or no click at all (using CI pipeline)</p>
-        <br /><br /><br /><br /><br />
-    </div>
-    </body>
-</html>
-'@
-Configuration WebServer {
+﻿Configuration WebServer {
     Import-DscResource -ModuleName PsDesiredStateConfiguration
     Node 'localhost'
     {   
@@ -48,3 +29,22 @@ Configuration WebServer {
         }    
     }
 }
+$html = @'
+<html>
+    <head><title>Azure Infrastructure</title>
+        <style>
+            .center{
+                margin: auto; width: 50%; border: 4px solid #93BD25; padding: 10px;
+            }
+        </style>
+    </head>
+    <body>
+    <div class="center" style="background-color:lightgray;">
+        <h3>A Journey to One Click of a Button</h3><hr />
+        <p>Or is it rather two clicks with <button type="button">Queue</button> and <button type="button"> Run </button></p>
+        <p>Or no click at all (using CI pipeline)</p>
+        <br /><br /><br /><br /><br />
+    </div>
+    </body>
+</html>
+'@
